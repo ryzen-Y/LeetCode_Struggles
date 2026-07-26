e9296cc2810f67aed94a354be9df12c19dac64c0
Mirza <mirzaaliuntauhid@gmail.com>
2026-07-27 00:29:16 +0600

    solution

--- leet/1732.py
+++ leet/1732.py
@@ -3,3 +3,11 @@ from typing import List
 
 class Solution:
     def largestAltitude(self, gain: List[int]) -> int:
+
+        line = []
+        line.append(0)
+
+        for i in gain:
+            ans += i
+            line.append(ans)
+        return max(line)
