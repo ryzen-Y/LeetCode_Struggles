609aed4f0e0133b4abb04fa8a833024301c91282
Mirza <mirzaaliuntauhid@gmail.com>
2026-07-26 23:35:39 +0600

    solution

---
+++ leet/303.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        self.prefix_sum = [0]
+
+        for i in nums:
+            self.prefix_sum.append(self.prefix_sum[-1] + i)
+
+    def sumRange(self, left: int, right: int) -> int:
+        return self.prefix_sum[right+1] - self.prefix_sum[left]
