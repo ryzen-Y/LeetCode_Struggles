6ba3199f811f4286dece018e56fb963e6927a287
ryzen <mirzaaliuntauhid@gmail.com>
2026-07-27 18:33:08 +0600

    solution

--- leet/53.py
+++ leet/53.py
@@ -3,3 +3,14 @@ from typing import List
 
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
+        prefix = [0]
+        min_prefix = 0
+        ans = nums[0]
+
+        for i in nums:
+            prefix.append(prefix[-1] + i)
+            current = prefix[-1] - min_prefix
+            ans = max(ans, current)
+            min_prefix = min(min_prefix, prefix[-1])
+
+        return ans
