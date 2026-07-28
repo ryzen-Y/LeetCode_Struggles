1fb0f4182cef433700e89c6ce892e86e91f2f80f
Mirza <mirzaaliuntauhid@gmail.com>
2026-07-29 00:41:28 +0600

    solution

--- leet/643.py
+++ leet/643.py
@@ -3,3 +3,15 @@ from typing import List
 
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
+        prefix_sum = [0]
+
+        for i in nums:
+            prefix_sum.append(prefix_sum[-1] + i)
+
+        max_sum = float('-inf')
+
+        for i in range(k, len(prefix_sum)):
+            current = prefix_sum[i] - prefix_sum[i - k]
+            max_sum = max(max_sum, current)
+
+        return max_sum / k
