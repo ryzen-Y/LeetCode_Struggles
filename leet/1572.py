fa840742e7fdba1f916c677792d4a663ac389503
ryzen <mirzaaliuntauhid@gmail.com>
2026-07-29 22:41:01 +0600

    solution

--- leet/1572.py
+++ leet/1572.py
@@ -3,3 +3,15 @@ from typing import List
 
 class Solution:
     def diagonalSum(self, mat: List[List[int]]) -> int:
+        n = len(mat)
+        first = 0
+        second = 0
+        for i in range(n):
+            first += mat[i][i]
+            second += mat[i][n-1-i]
+
+        if n % 2 != 0:
+            ans = (first + second) - mat[n // 2][n//2]
+            return ans
+        ans = first + second
+        return ans
