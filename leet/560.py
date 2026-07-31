3a075e3b7def48313aa52dc45a5ba7d258e0f5f9
ryzen <mirzaaliuntauhid@gmail.com>
2026-07-31 15:13:32 +0600

    :solution:

--- leet/560.py
+++ leet/560.py
@@ -3,3 +3,15 @@ from typing import List
 
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
+        prefix = 0
+        count = 0
+        seen = {0: 1}
+
+        for i in nums:
+            prefix = i + prefix
+            need = prefix - k
+
+            if need in seen:
+                count += seen[need]
+            seen[prefix] = seen.get(prefix, 0) + 1
+        return count
