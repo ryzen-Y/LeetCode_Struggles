da5e667140e4260e5422e51663497b605df2f137
Mirza <mirzaaliuntauhid@gmail.com>
2026-08-02 22:47:02 +0600

    solution

--- leet/3.py
+++ leet/3.py
@@ -1,2 +1,15 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+
+        left = 0
+        maximum = 0
+        current = set()
+
+        for i in range(len(s)):
+
+            while s[i] in current:
+                current.remove(s[left])
+                left += 1
+            current.add(s[i])
+            maximum = max(maximum, len(current))
+        return maximum
