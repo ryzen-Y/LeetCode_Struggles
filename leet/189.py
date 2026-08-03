33efff54c928a1837ab0233feea239cac5b0f17e
Mirza <mirzaaliuntauhid@gmail.com>
2026-08-04 00:55:47 +0600

    solution

--- leet/189.py
+++ leet/189.py
@@ -10,7 +10,7 @@ class Solution:
             while left < right:
                 nums[left], nums[right] = nums[right], nums[left]
                 left += 1
-                right += 1
+                right -= 1
 
         reverse(0, n-1)
         reverse(0, k - 1)
