b09b0a1fea7e1ee846c6bb9ac9bb8126d5afe353
ryzen <mirzaaliuntauhid@gmail.com>
2026-08-05 00:02:00 +0600

    solution

---
+++ leet/2469.py
@@ -0,0 +1,8 @@
+from typing import List
+
+
+class Solution(object):
+    def convertTemperature(self, celsius: float) -> List[float]:
+        Kelvin = celsius + 273.15
+        Fahrenheit = celsius * 1.80 + 32.00
+        return [Kelvin, Fahrenheit]
