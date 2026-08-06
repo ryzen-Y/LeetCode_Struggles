47ba3b6b1e011ce0b6e2e4eadd229393282ec4b3
ryzen <mirzaaliuntauhid@gmail.com>
2026-08-06 20:34:15 +0600

    solution

---
+++ leet/287.py
@@ -0,0 +1,13 @@
+from typing import List 
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        seen = set()
+        
+        for i in nums:
+            if i in seen:
+                return i
+            seen.add(i)
+                
+               
+        
