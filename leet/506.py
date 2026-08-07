5bb60ba1181e522bf0d305a70dd40d8a40382f28
ryzen <mirzaaliuntauhid@gmail.com>
2026-08-07 14:49:42 +0600

    solution

---
+++ leet/506.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findRelativeRanks(self, score: List[int]) -> List[str]:
+        sorted_score = sorted(score, reverse=True)
+
+        rank = {}
+        for i, s in enumerate(sorted_score):
+            if i == 0:
+                rank[s] = "Gold Medal"
+            elif i == 1:
+                rank[s] = "Silver Medal"
+            elif i == 2:
+                rank[s] = "Bronze Medal"
+            else:
+                rank[s] = str(i + 1)
+
+        ans = []
+        for s in score:
+            ans.append(rank[s])
+
+        return ans
