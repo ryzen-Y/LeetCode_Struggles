05ddb481b9ca3ef4c231cf1e040d9fd4ea16e679
Mirza <mirzaaliuntauhid@gmail.com>
2026-08-18 09:36:28 +0600

    solution

--- leet/3471.py
+++ leet/3471.py
@@ -1,6 +1,22 @@
-from typing import List\
+from typing import List
 
+## BRUTE FORCE ##
 
 
 class Solution:
     def largestInteger(self, nums: List[int], k: int) -> int:
+
+        seen = {}
+
+        for i in range(len(nums) - k+1):
+            window = set(nums[i:i+k])
+
+            for i in window:
+                seen[i] = seen.get(i, 0) + 1
+
+        ans = - 1
+
+        for i in seen:
+            if seen[i] == 1:
+                ans = max(ans, i)
+        return ans
