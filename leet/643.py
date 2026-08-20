1acccedfb0394cb59f179b5bc42e17aeeb0faf48
Mirza <mirzaaliuntauhid@gmail.com>
2026-08-21 02:44:41 +0600

    sliding window O[n]

--- leet/643.py
+++ leet/643.py
@@ -3,15 +3,23 @@ from typing import List
 
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
-        prefix_sum = [0]
+        prev_sum = 0
 
-        for i in nums:
-            prefix_sum.append(prefix_sum[-1] + i)
+        for x in range(k):
+            prev_sum += nums[x]
 
-        max_sum = float('-inf')
+        max_sum = prev_sum
 
-        for i in range(k, len(prefix_sum)):
-            current = prefix_sum[i] - prefix_sum[i - k]
-            max_sum = max(max_sum, current)
+        i = 1
+        j = k
+
+        while j < len(nums):
+            prev_sum = prev_sum + nums[j] - nums[i - 1]
+
+            if prev_sum > max_sum:
+                max_sum = prev_sum
+
+            i += 1
+            j += 1
 
         return max_sum / k
