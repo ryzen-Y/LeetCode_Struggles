4c94b6205d0bae21df81e57bf07e76ba7903bd1d
ryzen <mirzaaliuntauhid@gmail.com>
2026-08-20 19:41:01 +0600

    O[n]

--- leet/3069.py
+++ leet/3069.py
@@ -3,13 +3,12 @@ from typing import List
 
 class Solution:
     def resultArray(self, nums: List[int]) -> List[int]:
-        arr1 = []
-        arr2 = []
+        arr1 = [nums[0]]
+        arr2 = [nums[1]]
 
-        for i in range(len(nums)):
-            if i % 2 == 0:
+        for i in range(2, len(nums)):
+            if arr1[-1] > arr2[-1]:
                 arr1.append(nums[i])
             else:
                 arr2.append(nums[i])
-
-        return arr1 + arr2
+        return arr1+arr2
