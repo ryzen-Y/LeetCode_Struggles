b4b2b1904b27665b232d10c87a4ec5b4e0cfa964
ryzen <mirzaaliuntauhid@gmail.com>
2026-08-27 14:43:59 +0600

    solution

--- leet/3720.py
+++ leet/3720.py
@@ -1,2 +1,33 @@
 class Solution:
     def lexGreaterPermutation(self, s: str, target: str) -> str:
+        ans = ""
+        s = sorted(list(s))
+
+        for i in range(len(s)):
+
+            if target[i] in s:
+                ans += target[i]
+                s.remove(target[i])
+
+            else:
+                for ch in s:
+                    if ch > target[i]:
+                        ans += ch
+                        s.remove(ch)
+                        return ans + "".join(s)
+
+                break
+
+        for i in range(len(ans) - 1, -1, -1):
+
+            s.append(ans[i])
+            s.sort()
+
+            for ch in s:
+                if ch > ans[i]:
+                    new_ans = ans[:i] + ch
+                    s.remove(ch)
+
+                    return new_ans + "".join(s)
+
+        return ""
