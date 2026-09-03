403428a2035d19e04476b2182485f7aed9f2aa09
ryzen <mirzaaliuntauhid@gmail.com>
2026-09-03 12:56:35 +0600

    sol

--- leet/3876.py
+++ leet/3876.py
@@ -1,2 +1,38 @@
 class Solution:
-    def uniformArray(self, nums1: list[int]) -> bool:
+    def uniformArray(self, num1: list[int]) -> bool:
+
+        if num1[0] % 2 != 0:
+            flag = True
+            for i in range(len(num1)):
+                if i == 0:
+                    continue
+
+                if num1[i] % 2 == 0:
+                    found = False
+                    for j in range(len(num1)):
+                        if i != j:
+                            diff = num1[i] - num1[j]
+                            if diff % 2 == 1 and diff >= 1:
+                                found = True
+                                break
+                    if found == False:
+                        flag = False
+            return flag
+
+        else:
+            flag = True
+            for i in range(len(num1)):
+                if i == 0:
+                    continue
+
+                if num1[i] % 2 != 0:
+                    found = False
+                    for j in range(len(num1)):
+                        if i != j:
+                            diff = num1[i] - num1[j]
+                            if diff % 2 == 1 and diff >= 1:
+                                found = True
+                                break
+                    if found == False:
+                        flag = False
+            return flag
