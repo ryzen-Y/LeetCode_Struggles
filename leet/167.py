bb0c352cf9266fa1f9d8ac7748a7286f1d78b507
ryzen <mirzaaliuntauhid@gmail.com>
2026-09-04 01:52:16 +0600

    two pointers

--- leet/167.py
+++ leet/167.py
@@ -1,3 +1,14 @@
+from typing import List
+
+
 class Solution:
-    def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        
+    def twoSum(self, num: List[int], target: int) -> List[int]:
+        left, right = 0, len(num) - 1
+
+        while left < right:
+            if num[left] + num[right] == target:
+                return left, right
+            elif num[left] + num[right] < target:
+                left += 1
+            else:
+                right -= 1
