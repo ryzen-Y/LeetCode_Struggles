5498d9c4257cd5ecdacb49fa08b53167d6b4010c
ryzen <mirzaaliuntauhid@gmail.com>
2026-09-04 02:25:51 +0600

    sol

---
+++ leet/10.py
@@ -0,0 +1,8 @@
+from typing import List
+
+
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        for i in range(len(nums)):
+            nums[i] = nums[i] * nums[i]
+        return (sorted(nums))
