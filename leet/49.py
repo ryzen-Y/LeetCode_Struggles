f484075fd2c6137183f57c755906694c9d74337c
ryzen <mirzaaliuntauhid@gmail.com>
2026-09-04 11:57:33 +0600

    sol

--- leet/49.py
+++ leet/49.py
@@ -3,3 +3,13 @@ from typing import List
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        seen = {}
+
+        for words in strs:
+            key = tuple(sorted(words))
+
+            if key not in seen:
+                seen[key] = []
+            seen[key].append(words)
+        return list(seen.values())
