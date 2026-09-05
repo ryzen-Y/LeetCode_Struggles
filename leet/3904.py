005b6905b25f1db8b565061c768a30347e4a28f0
ryzen <mirzaaliuntauhid@gmail.com>
2026-09-05 16:25:56 +0600

    sol o{n}

--- leet/3904.py
+++ leet/3904.py
@@ -1,2 +1,19 @@
 class Solution:
     def firstStableIndex(self, nums: list[int], k: int) -> int:
+        n = len(nums)
+
+        suff = [0] * n
+        suff[n - 1] = nums[n - 1]
+
+        for i in range(n - 2, -1, -1):
+            suff[i] = min(suff[i + 1], nums[i])
+
+        pref = nums[0]
+
+        for i in range(n):
+            pref = max(pref, nums[i])
+
+            if pref - suff[i] <= k:
+                return i
+
+        return -1
