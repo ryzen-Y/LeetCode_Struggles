c8f22d2092b0c1e861b946022f0a298386b629ad
ryzen <mirzaaliuntauhid@gmail.com>
2026-09-09 23:58:19 +0600

    Sol

--- leet/88.py
+++ leet/88.py
@@ -6,3 +6,22 @@ class Solution:
         for i in range(n):
             nums1[m+i] = nums2[i]
         nums1.sort()
+
+
+# ==== Two pointers ===
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        i = m - 1
+        j = n - 1
+        index = m + n - 1
+
+        while j != -1:
+
+            if i >= 0 and nums1[i] > nums2[j]:
+                nums1[index] = nums1[i]
+                i -= 1
+            else:
+                nums1[index] = nums2[j]
+                j -= 1
+            index -= 1
