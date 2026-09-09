f485559edf8122e76077ae1235ac853a9d17fa6c
ryzen <mirzaaliuntauhid@gmail.com>
2026-09-09 21:44:10 +0600

    sol

--- leet/1768.py
+++ leet/1768.py
@@ -12,3 +12,25 @@ class Solution:
             s = s + word2[n:]
 
         return s
+
+
+# ======= Two pointers ======
+
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        i = 0
+        j = 0
+        s = ""
+
+        word1_len = len(word1)
+        word2_len = len(word2)
+
+        while i < word1_len or j < word2_len:
+
+            if i < word1_len:
+                s = s + word1[i]
+                i += 1
+            if j < word2_len:
+                s = s + word2[j]
+                j += 1
+        return s
