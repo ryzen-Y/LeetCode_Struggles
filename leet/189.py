e31b66d007c7a9cfaabccc0a095267fdb8bec7a8
ryzen <mirzaaliuntauhid@gmail.com>
2026-09-14 13:24:39 +0600

    sol

--- leet/189.py
+++ leet/189.py
@@ -18,3 +18,17 @@ class Solution:
         self.reverse(nums, 0, n - 1)
         self.reverse(nums, k, n - 1)
         self.reverse(nums, 0, k - 1)
+
+
+# ======== Slicing Method ========
+
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        n = len(nums)
+        k %= n
+
+        if k != 0:
+            arr = [n-k:]
+            del nums[n-k:]
+            nums[0:0] = arr
