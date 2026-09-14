2da6f915fd8a64e155ead89cbfa67ba6f6069dce
ryzen <mirzaaliuntauhid@gmail.com>
2026-09-14 11:47:58 +0600

    sol

--- leet/18.py
+++ leet/18.py
@@ -1,15 +1,16 @@
 class Solution:
     def fourSum(self, nums: list[int], target: int) -> list[list[int]]:
 
-        result = []
-        nums.sort()
         n = len(nums)
+        nums.sort()
+        result = []
 
         for i in range(n):
 
-            if i > 0 and nums[i] == nums[i-1]:
+            if i > 0 and nums[i] == nums[i - 1]:
                 continue
-            for j in range(i + 1, n):
+
+            for j in range(i+1, n):
 
                 if j > i + 1 and nums[j] == nums[j - 1]:
                     continue
@@ -17,6 +18,7 @@ class Solution:
                 right = n - 1
 
                 while left < right:
+
                     x = nums[i] + nums[j] + nums[left] + nums[right]
 
                     if x == target:
@@ -24,14 +26,13 @@ class Solution:
                             [nums[i], nums[j], nums[left], nums[right]])
                         left += 1
                         right -= 1
-
                         while left < right and nums[left] == nums[left - 1]:
                             left += 1
                         while left < right and nums[right] == nums[right + 1]:
                             right -= 1
+
                     elif x < target:
                         left += 1
                     else:
                         right -= 1
-
         return result
