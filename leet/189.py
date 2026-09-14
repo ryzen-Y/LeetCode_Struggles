f53b1da57c1e010ae678e8baf7c5cf19bc539927
ryzen <mirzaaliuntauhid@gmail.com>
2026-09-14 13:02:29 +0600

    Sol

--- leet/189.py
+++ leet/189.py
@@ -2,16 +2,19 @@ from typing import List
 
 
 class Solution:
+    def reverse(self, nums, left, right):
+
+        while left < right:
+
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
+
     def rotate(self, nums: List[int], k: int) -> None:
+
         n = len(nums)
+
         k = k % n
-
-        def reverse(left, right):
-            while left < right:
-                nums[left], nums[right] = nums[right], nums[left]
-                left += 1
-                right -= 1
-
-        reverse(0, n-1)
-        reverse(0, k - 1)
-        reverse(k, n - 1)
+        self.reverse(nums, 0, n - 1)
+        self.reverse(nums, k, n - 1)
+        self.reverse(nums, 0, k - 1)
