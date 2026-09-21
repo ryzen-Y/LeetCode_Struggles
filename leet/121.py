0ef2462a336f2ca67a51b97e2def13f095ff53a7
Mirza <mirzaaliuntauhid@gmail.com>
2026-09-21 10:27:47 +0600

    sol

--- leet/121.py
+++ leet/121.py
@@ -10,3 +10,23 @@ class Solution:
             profit = i - min_price
             max_profit = max(profit, max_profit)
         return max_profit
+
+# sliding window approch
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+
+        buy = 0
+        sell = 1
+        n = len(prices)
+        maximum = 0
+
+        while sell < n:
+
+            if prices[sell] < prices[buy]:
+                buy = sell
+            else:
+                profit = prices[sell] - prices[buy]
+                maximum = max(maximum, profit)
+        return maximum
