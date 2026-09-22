f0a2dfb4b5140ab620964f830045f5f4516e619c
Mirza <mirzaaliuntauhid@gmail.com>
2026-09-22 10:41:48 +0600

    second sol

--- leet/3.py
+++ leet/3.py
@@ -13,3 +13,21 @@ class Solution:
             current.add(s[i])
             maximum = max(maximum, len(current))
         return maximum
+
+
+# sliding window
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        left = 0
+        maximum = 0
+        seen = set()
+
+        for right in range(len(s)):
+            while s[right] in seen:
+                seen.remove(s[left])
+                left += 1
+
+            seen.add(s[right])
+            maximum = max(maximum, right - left + 1)
+        return maximum
